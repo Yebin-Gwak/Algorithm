79018c94a84af295461af73e2bbdcf58f4cbba8a
Yebin Gwak <ferf99@naver.com>
2023-10-21 04:07:10 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 212 ms, Memory: 63,296 KB -BaekjoonHub

---
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.py
@@ -0,0 +1,34 @@
+for test_case in range(1, 11):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    M = int(input())
+    cmdList = list(input().split())
+    index = 0
+
+    for i in range(M):
+        cmd = cmdList[index]
+        if cmd == 'I':
+            x = int(cmdList[index + 1])
+            y = int(cmdList[index + 2])
+            for j in range(index + 2 + y, index + 2, -1):
+                arr.insert(x, cmdList[j])
+            index += 3 + y
+
+        elif cmd == 'D':
+            x = int(cmdList[index + 1])
+            y = int(cmdList[index + 2])
+            for j in range(y):
+                arr.pop(x)
+            index += 3
+
+        elif cmd == 'A':
+            y = int(cmdList[index + 1])
+            
+            for j in range(index + 2, index + 2 + y):
+                arr.append(cmdList[j])
+            index += 2 + y
+            
+    print(f'#{test_case}', end=' ')
+    for k in range(10):
+        print(arr[k], end=' ')
+    print()
