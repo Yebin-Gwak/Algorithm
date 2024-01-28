09de0309cd2a25f550fa4321df58224dcce366f8
Yebin Gwak <ferf99@naver.com>
2024-01-28 23:35:58 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 200 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		int[] arr = new int[9];
+		for (int i = 0; i < 9; i++) {
+			int value = Integer.parseInt(sc.nextLine());
+			arr[i] = value;
+			sum += value;
+		}
+
+		for (int i = 0; i < 8; i++) {
+			for (int j = i + 1; j < 9; j++) {
+				if (sum - (arr[i] + arr[j]) == 100) {
+					arr[i] = 0;
+					arr[j] = 0;
+					Arrays.sort(arr);
+					for (int k = 2; k < 9; k++)
+						System.out.println(arr[k]);
+					return;
+				}
+			}
+		}
+
+	}
+
+}
