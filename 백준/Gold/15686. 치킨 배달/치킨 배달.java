5134001901d373a7753e0bcec4c5575859c793d0
Yebin Gwak <ferf99@naver.com>
2024-01-31 22:20:24 +0900

    [Gold V] Title: 치킨 배달, Time: 244 ms, Memory: 16928 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N, M, shopCount;
+	static int N, M, ans;
 	static int[][] map;
 
 	static List<int[]> shopIndex = new ArrayList<>();
-	static List<int[]> shopAddress = new ArrayList<>();
-	static List<int[]> homeAddress = new ArrayList<>();
+	static List<int[]> shop = new ArrayList<>();
+	static List<int[]> home = new ArrayList<>();
 	static int[] input;
 	static boolean[] visited;
 
@@ -20,40 +20,26 @@ public class Main {
 
 		map = new int[N][N];
 		input = new int[M];
-		int ans = Integer.MAX_VALUE;
+		ans = Integer.MAX_VALUE;
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 				if (map[i][j] == 1)
-					homeAddress.add(new int[] { i, j });
+					home.add(new int[] { i, j });
 				else if (map[i][j] == 2) {
-					shopAddress.add(new int[] { i, j });
-					shopCount++;
+					shop.add(new int[] { i, j });
+
 				}
 			}
 		}
-
-		visited = new boolean[shopCount];
+		
+		visited = new boolean[shop.size()];
 
 		comb(0, 0);
 
-		for (int i = 0; i < shopIndex.size(); i++) {
-			int tempAllDistance = 0;
-			for (int j = 0; j < homeAddress.size(); j++) {
-				int tempDistance = Integer.MAX_VALUE;
-				for (int k = 0; k < M; k++) { // 현재 집에서 가장 가까운 치킨집과의 거리 저장
-					int tempSize = Math.abs(homeAddress.get(j)[0] - shopAddress.get(shopIndex.get(i)[k])[0])
-							+ Math.abs(homeAddress.get(j)[1] - shopAddress.get(shopIndex.get(i)[k])[1]);
-					if (tempSize < tempDistance)
-						tempDistance = tempSize;
-				}
-				tempAllDistance += tempDistance;
-			}
-			if (tempAllDistance < ans)
-				ans = tempAllDistance; 
-		}
+		calc();
 
 		System.out.println(ans);
 
@@ -65,12 +51,11 @@ public class Main {
 			for (int j = 0; j < M; j++) {
 				tempArr[j] = input[j];
 			}
-
 			shopIndex.add(tempArr);
 			return;
 		}
 
-		for (int i = start; i < shopCount; i++) {
+		for (int i = start; i < shop.size(); i++) {
 			if (visited[i])
 				continue;
 			visited[i] = true;
@@ -78,7 +63,23 @@ public class Main {
 			comb(cnt + 1, i + 1);
 			visited[i] = false;
 		}
-
 	}
 
+	private static void calc() {
+		for (int i = 0; i < shopIndex.size(); i++) {
+			int totalSize = 0;
+			for (int j = 0; j < home.size(); j++) {
+				int homeSize = Integer.MAX_VALUE; 
+				for (int k = 0; k < M; k++) {
+					int tempSize = Math.abs(home.get(j)[0] - shop.get(shopIndex.get(i)[k])[0])
+							+ Math.abs(home.get(j)[1] - shop.get(shopIndex.get(i)[k])[1]);
+					if (tempSize < homeSize)
+						homeSize = tempSize;
+				}
+				totalSize += homeSize; 
+			}
+			if (totalSize < ans)
+				ans = totalSize; 
+		}
+	}
 }
