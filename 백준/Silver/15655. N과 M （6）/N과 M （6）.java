8518082514f942cefaed5e6862a8251628d16c7b
Yebin Gwak <ferf99@naver.com>
2024-02-23 23:40:09 +0900

    [Silver III] Title: N과 M (6), Time: 120 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N, M;
+	static int[] numbers;
+	static boolean[] visited;
+	static int[] input;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		numbers = new int[N];
+		visited = new boolean[N];
+		input = new int[M];
+		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < N; i++)
+			numbers[i] = Integer.parseInt(st.nextToken());
+
+		Arrays.sort(numbers);
+		
+		permutation(0, 0);
+
+		System.out.print(sb.toString());
+
+	}
+	
+	private static void permutation(int cnt, int start) {
+		if(cnt == M) {
+			for(int x : input)
+				sb.append(x).append(" ");
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = start; i < N; i++) {
+			if(visited[i]) continue;
+			
+			visited[i] = true;
+			input[cnt] = numbers[i];
+			permutation(cnt + 1, i + 1);
+			
+			visited[i] = false;
+		}
+	}
+
+}
