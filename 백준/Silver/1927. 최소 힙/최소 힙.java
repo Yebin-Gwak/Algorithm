b11ca80ec729790ffcf81dae9a817b5b138f7a41
Yebin Gwak <ferf99@naver.com>
2024-04-10 23:38:06 +0900

    [Silver II] Title: 최소 힙, Time: 388 ms, Memory: 27404 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>((o1, o2) -> o1 - o2);
+		
+		for(int i = 0; i < N; i++) {
+			int x = Integer.parseInt(br.readLine());
+			if(x == 0) {
+				if(!pq.isEmpty())
+					sb.append(pq.poll() + "\n");
+				else 
+					sb.append(0 + "\n");
+				
+			}else {
+				pq.add(x);
+			}
+		}
+		System.out.println(sb.toString());
+	}
+}
