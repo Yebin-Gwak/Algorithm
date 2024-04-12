7747fbce740bcadb781e2e575c1ca1ed24de30b4
Yebin Gwak <ferf99@naver.com>
2024-04-12 14:57:26 +0900

    [Silver I] Title: 절댓값 힙, Time: 352 ms, Memory: 26616 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>((o1, o2) -> {
+			if(Math.abs(o1) == Math.abs(o2))
+				return o1 - o2;
+			 return Math.abs(o1) - Math.abs(o2);
+					 });
+		
+		for(int i = 0; i < N; i++) {
+			int x = Integer.parseInt(br.readLine());
+			if(x == 0) {
+				if(!pq.isEmpty())
+					sb.append(pq.poll() + "\n");
+				else 
+					sb.append(0 + "\n");
+				
+			}else {
+				pq.add(x);
+			}
+		}
+		System.out.println(sb.toString());
+	}
+}
