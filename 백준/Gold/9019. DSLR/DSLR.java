f9f330be24cd789b06b2433299bdeab7f081a689
Yebin Gwak <ferf99@naver.com>
2024-04-30 14:38:23 +0900

    [Gold IV] Title: DSLR, Time: 4480 ms, Memory: 301036 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -9,17 +9,17 @@ public class Main {
 			this.value = value;
 			this.history = history;
 		}
-		
 	}
 	static StringBuilder sb = new StringBuilder();
 	static int start, end;
 	static Deque<Num> deque = new ArrayDeque<>();
 	static boolean[] visited;
+	static char[] oper = {'D', 'S', 'L', 'R'};
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		int T = Integer.parseInt(br.readLine());
-		for(int tc = 0; tc < T; tc++) {
+		for(int i = 0; i < T; i++) {
 			st = new StringTokenizer(br.readLine());
 			start = Integer.parseInt(st.nextToken());
 			end = Integer.parseInt(st.nextToken());
@@ -41,53 +41,24 @@ public class Main {
 				sb.append("\n");
 				return;
 			}
-			operD(num);
-			operS(num);
-			operL(num);
-			operR(num);
+			for(int i = 0; i < 4; i++) {
+				int value = calc(num.value, i);
+				
+				if(visited[value]) continue;
+				visited[value] = true;
+				
+				ArrayList<Character> list = new ArrayList<>(num.history);
+				list.add(oper[i]);
+				deque.add(new Num(value, list));
+			}
 		}
 	}
 	
-	private static void operD(Num num) {
-		int value = (num.value * 2) % 10000;
-		if(visited[value]) return;
-		visited[value] = true;
-		
-		ArrayList<Character> list = new ArrayList<>(num.history);
-		list.add('D');
-		deque.add(new Num(value, list));
-	}
-	
-	private static void operS(Num num) {
-		int value = (num.value == 0) ? 9999 : num.value - 1;
-		if(visited[value]) return;
-		visited[value] = true;
-		
-		ArrayList<Character> list = new ArrayList<>(num.history);
-		list.add('S');
-		deque.add(new Num(value, list));
-	}
-
-	private static void operL(Num num) {
-		int value = (num.value % 1000) * 10 + (num.value / 1000);
-
-		if(visited[value]) return;
-		visited[value] = true;
-
-		ArrayList<Character> list = new ArrayList<>(num.history);
-		list.add('L');
-		deque.add(new Num(value, list));
-	}
-
-	private static void operR(Num num) {
-		int value = (num.value % 10) * 1000 + (num.value / 10);
-				
-		if(visited[value]) return;
-		visited[value] = true;
-		
-		ArrayList<Character> list = new ArrayList<>(num.history);
-		list.add('R');
-		deque.add(new Num(value, list));
+	private static int calc(int value, int oper) {
+		if(oper == 0) return (value * 2) % 10000;
+		if(oper == 1) return (value == 0) ? 9999 : value - 1;
+		if(oper == 2) return (value % 1000) * 10 + (value / 1000);
+		else return (value % 10) * 1000 + (value / 10);
 	}
 
 }
