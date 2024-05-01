7df2e17d526842eab3308701ecee489c471b2819
Yebin Gwak <ferf99@naver.com>
2024-05-01 15:42:34 +0900

    [Gold II] Title: 2048 (Easy), Time: 264 ms, Memory: 20932 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -5,16 +5,12 @@ public class Main {
 	static int N;
 	static int[][] originMap;
 	static boolean[][] summed;
-	// 하 상 우 좌
+
 	static int[] dx = {1, -1, 0, 0};
 	static int[] dy = {0, 0, 1, -1};
 	
-	static int[] startX;
-	static int[] endX;
-	static int[] indexX;
-	static int[] startY;
-	static int[] endY;
-	static int[] indexY;
+	static int[] startX, endX, indexX;
+	static int[] startY, endY, indexY;
 	
 	static int ans = 0;
 	
@@ -36,22 +32,20 @@ public class Main {
 		startY = new int[] {0, 0, N - 1, 0};
 		endY = new int[] {N, N, -1, N};
 		indexY = new int[]{1, 1, -1, 1};
+		
 		dfs(0, originMap);
 		System.out.println(ans);
 	}
 
-	private static void dfs(int cnt, int[][] originMap) {
+	private static void dfs(int cnt, int[][] map) {
 		if(cnt == 5) {
-			calc(originMap);
+			calc(map);
 			return;
 		} 
 		
-		for(int i = 0; i < 4; i++) {
-			int[][] resultMap = move(originMap, i);
-			dfs(cnt + 1, resultMap);
-		}
+		for(int i = 0; i < 4; i++) 
+			dfs(cnt + 1, move(map, i));
 	}
-	
 
 	private static int[][] move(int[][] tempMap, int d) {
 		int[][] map = new int[N][];
@@ -60,58 +54,46 @@ public class Main {
 		
 		summed = new boolean[N][N];
 		int sX = startX[d];
-		
 		while(sX != endX[d]) {
 			int sY = startY[d];
 			while(sY != endY[d]) {
 				int x = sX;
 				int y = sY;
 				int value = map[x][y];
+	
 				if(value == 0) {
 					sY += indexY[d];
 					continue;
 				}
-				
 				while(true) {
 					x += dx[d];
 					y += dy[d];
 					if(0 <= x && x < N && 0 <= y && y < N) {
-						// 다음 칸이 0일때
 						if(map[x][y] == 0) {
-							// 다다음칸이 존재한다면 계속 내림
-							if(0 <= x + dx[d] && x + dx[d] < N && 0 <= y + dy[d] && y + dy[d] < N) {
+							if(0 <= x + dx[d] && x + dx[d] < N && 0 <= y + dy[d] && y + dy[d] < N)
 								continue;
-							}
-							else {
-								map[x][y] = value;
-								map[sX][sY] = 0;
-								break;
-							}
+							map[x][y] = value;
+							map[sX][sY] = 0;
+							break;
 						}
-						// 다음 칸이 0이 아닐 때
 						else {
-							// 다음칸 값이 나와 같고, 해당 칸이 아직 안 합쳐졌었다면
 							if(value == map[x][y] && !summed[x][y]) {
 								summed[x][y] = true;
 								map[sX][sY] = 0;
 								map[x][y] = value * 2;
 								break;
 							}
-							else {
-								map[sX][sY] = 0;
-								map[x - dx[d]][y - dy[d]] = value;
-								break;
-							}
+							map[sX][sY] = 0;
+							map[x - dx[d]][y - dy[d]] = value;
+							break;
 						}
 					}
-					else break;
+					break;
 				}
 				sY += indexY[d];
-
 			}
 			sX += indexX[d];
 		}
-			
 		return map;
 	}
 
@@ -119,9 +101,5 @@ public class Main {
 		for(int[] arr : map)
 			for(int x : arr)
 				ans = Math.max(ans, x);
-		
-		if(ans == 128) {
-			System.out.println(1);
-		}
 	}
 }
