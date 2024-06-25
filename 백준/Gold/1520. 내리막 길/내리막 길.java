5877b008b19255db019518cbbbc4014f594bcc83
Yebin Gwak <ferf99@naver.com>
2024-06-25 23:45:40 +0900

    [Gold III] Title: 내리막 길, Time: 436 ms, Memory: 36540 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -2,9 +2,9 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static class Pos implements Comparable<Pos>{
+	static class Pos implements Comparable<Pos> {
 		int x, y, size;
-		
+
 		public Pos(int x, int y, int size) {
 			this.x = x;
 			this.y = y;
@@ -15,66 +15,62 @@ public class Main {
 		public int compareTo(Pos o) {
 			return o.size - this.size;
 		}
-		
+
 	}
-	
+
 	static int N, M;
 	static PriorityQueue<Pos> pq = new PriorityQueue<>();
 	static int[][] map;
 	static int[][] dp;
-	static int[] dx = {1, -1, 0, 0};
-	static int[] dy = {0, 0, 1, -1};
+	static int[] dx = { 1, -1, 0, 0 };
+	static int[] dy = { 0, 0, 1, -1 };
 	static int ans = 0;
-	
-	public static void main(String[] args) throws IOException{
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		if(N == 1 && M == 1) {
+		if (N == 1 && M == 1) {
 			System.out.println(1);
 			return;
 		}
-		
+
 		map = new int[N][M];
 		dp = new int[N][M];
-		
-		for(int i = 0; i < N; i++) {
+
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < M; j++) {
+			for (int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
+
 		pq.add(new Pos(0, 0, map[0][0]));
 		dp[0][0] = 1;
-		
+
 		calc();
 		System.out.println(dp[N - 1][M - 1]);
 	}
-	
+
 	private static void calc() {
-		while(!pq.isEmpty()) {
+		while (!pq.isEmpty()) {
 			Pos now = pq.poll();
 			int x = now.x;
 			int y = now.y;
-			
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
 
-                if (0 <= nx && nx < N && 0 <= ny && ny < M) {
-                    if (map[x][y] > map[nx][ny]) { // 내리막길로 이동
-                        if (dp[nx][ny] == 0) {
-                            dp[nx][ny] = 0;
-                            pq.add(new Pos(nx, ny, map[nx][ny]));
-                        }
-                        dp[nx][ny] += dp[x][y];
-                    }
-                }
-            }
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+
+				if (0 <= nx && nx < N && 0 <= ny && ny < M && map[x][y] > map[nx][ny]) {
+					if (dp[nx][ny] == 0)
+						pq.add(new Pos(nx, ny, map[nx][ny]));
+					dp[nx][ny] += dp[x][y];
+				}
+			}
 		}
-		
+
 	}
-	
+
 }
