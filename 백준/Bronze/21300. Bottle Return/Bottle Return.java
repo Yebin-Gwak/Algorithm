0224f76c7afa472f26a29c13d8653a96ff8fd39c
Yebin Gwak <ferf99@naver.com>
2024-08-27 23:52:24 +0900

    [Bronze V] Title: Bottle Return, Time: 164 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int s = 0;
+		for(int i = 0; i < 6; i++)
+			s += sc.nextInt();
+		System.out.println(s * 5);
+	}
+}
