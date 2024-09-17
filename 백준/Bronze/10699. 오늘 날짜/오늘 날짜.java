daa92505df5d2e6063e7b67c48f7fe3c2b91b6df
Yebin Gwak <ferf99@naver.com>
2024-09-17 21:55:03 +0900

    [Bronze V] Title: 오늘 날짜, Time: 152 ms, Memory: 17036 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("2024-09-17");
+	}
+}
