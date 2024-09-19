9c52b6325ca0fe1ecccc54b5c3bfa188244b6392
Yebin Gwak <ferf99@naver.com>
2024-09-19 22:07:37 +0900

    [Bronze V] Title: 등록, Time: 160 ms, Memory: 17168 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(440);
+		System.out.println("ferf99");
+	}
+}
