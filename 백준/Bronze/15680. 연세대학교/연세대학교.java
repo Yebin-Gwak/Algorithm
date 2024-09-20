79eebae2bfe662b9603b3b0554e9aa25b9e02970
Yebin Gwak <ferf99@naver.com>
2024-09-20 09:31:37 +0900

    [Bronze V] Title: 연세대학교, Time: 160 ms, Memory: 17556 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		if(sc.nextInt() == 0)
+			System.out.println("YONSEI");
+		else
+			System.out.println("Leading the Way to the Future");
+	}
+}
