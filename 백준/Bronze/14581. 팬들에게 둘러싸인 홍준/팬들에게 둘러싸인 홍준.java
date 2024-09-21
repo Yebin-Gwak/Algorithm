4e6624699ca061fe90d7adc34eb7e0480ae49cf6
Yebin Gwak <ferf99@naver.com>
2024-09-21 23:54:08 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 176 ms, Memory: 18188 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::" + sc.next() + "::fan:");
+		System.out.println(":fan::fan::fan:");
+	}
+}
