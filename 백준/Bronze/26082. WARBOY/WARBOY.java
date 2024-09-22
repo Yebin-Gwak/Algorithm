d9af67d7000843aab7c40e293486c6f3d7ab7962
Yebin Gwak <ferf99@naver.com>
2024-09-22 17:56:42 +0900

    [Bronze V] Title: WARBOY, Time: 164 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+		int d = b / a * 3;
+		System.out.println(d * c);
+
+	}
+}
