d535a1ef3b513f7947d3773290a74b0c69642873
Yebin Gwak <ferf99@naver.com>
2024-09-29 20:11:06 +0900

    [Bronze V] Title: CASIO, Time: 168 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		System.out.println((sc.nextLong() == sc.nextLong()) ? 1 : 0);
+	}
+}
