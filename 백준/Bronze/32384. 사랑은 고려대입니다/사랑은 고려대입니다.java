21b9b1fba1b66aac213f1e8092281a9f0ef66649
Yebin Gwak <ferf99@naver.com>
2024-10-07 23:32:24 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 164 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 0; i < n; i++)
+			System.out.print("LoveisKoreaUniversity ");
+	}
+}
