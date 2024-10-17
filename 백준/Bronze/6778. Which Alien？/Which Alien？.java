e4209c31fe95ff29ce8dc96631b9f030b01c407f
Yebin Gwak <ferf99@naver.com>
2024-10-17 17:31:33 +0900

    [Bronze IV] Title: Which Alien?, Time: 96 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		if(a >= 3 && b <= 4)
+			System.out.println("TroyMartian");
+		if(a <= 6 && b >= 2)
+			System.out.println("VladSaturnian");
+		if(a <= 2 && b <= 3)
+			System.out.println("GraemeMercurian");
+	}
+}
