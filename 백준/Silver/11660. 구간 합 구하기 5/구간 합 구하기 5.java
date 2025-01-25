28a0dcbdd79fe1030810a7d7f5fc9555f254dc97
Yebin Gwak <ferf99@naver.com>
2025-01-25 10:19:22 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 776 ms, Memory: 123540 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,43 +1,40 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-	static int[][] arr;
-	static StringBuilder sb = new StringBuilder();
+public class Main{
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-
-		arr = new int[N + 1][N + 1];
-
-		for (int i = 1; i <= N; i++) {
+		
+		int[][] arr = new int[N + 1][N + 1];
+		
+		for(int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= N; j++)
-				arr[i][j] = arr[i][j - 1] + Integer.parseInt(st.nextToken());
+			for(int j = 1; j <= N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
 		}
-
-		for (int i = 0; i < M; i++) {
+		
+		int[][] sum = new int[N + 1][N + 1];
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= N; j++) {
+				sum[i][j] = sum[i][j - 1] + sum[i - 1][j] - sum[i - 1][j - 1] + arr[i][j];
+			}
+		}
+		
+		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int x1 = Integer.parseInt(st.nextToken());
-			int y1 = Integer.parseInt(st.nextToken());
-			int x2 = Integer.parseInt(st.nextToken());
-			int y2 = Integer.parseInt(st.nextToken());
-
-			calc(x1, y1, x2, y2);
+			int sX = Integer.parseInt(st.nextToken());
+			int sY = Integer.parseInt(st.nextToken());
+			int eX = Integer.parseInt(st.nextToken());
+			int eY = Integer.parseInt(st.nextToken());
+			sb.append(sum[eX][eY] - sum[sX - 1][eY] - sum[eX][sY - 1] + sum[sX - 1][sY - 1]).append("\n");
 		}
-		System.out.println(sb.toString());
-
-	}
-
-	private static void calc(int x1, int y1, int x2, int y2) {
-		int sum = 0;
-
-		for (int i = x1; i <= x2; i++) {
-			sum += arr[i][y2] - arr[i][y1 - 1];
-		}
-		sb.append(sum + "\n");
+		System.out.print(sb.toString());
 	}
 }
