c27ed180c0517a76cef38dbc4d88e7672e9ce9ad
Yebin Gwak <ferf99@naver.com>
2025-01-27 16:05:10 +0900

    [Silver II] Title: DFS와 BFS, Time: 248 ms, Memory: 20900 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,67 +2,63 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static int[][] arr;
+	static ArrayList<Integer>[] lists;
 	static boolean[] visited;
-	
-	
-	public static void main(String[] args) throws IOException{
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		int V = Integer.parseInt(st.nextToken());
-		arr = new int[N+1][N+1];
-		
-		visited = new boolean[N+1];
-		
+		lists = new ArrayList[N + 1];
+		for(int i = 0; i <= N; i++)
+			lists[i] = new ArrayList<>();
 		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			arr[x][y] = 1;
-			arr[y][x] = 1;
-			
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			lists[s].add(e);
+			lists[e].add(s);
 		}
+		for(ArrayList list : lists)
+			Collections.sort(list);
+		
+		visited = new boolean[N + 1];
 		dfs(V);
 		sb.append("\n");
-		visited = new boolean[N+1];
+		visited = new boolean[N + 1];
 		bfs(V);
 		System.out.println(sb.toString());
-		
-
 	}
-	
-	
-	private static void dfs(int start) {
-		sb.append(start + " ");
-		visited[start] = true;
-		for(int i = 1; i < arr.length; i++) {
-			if(arr[start][i] == 1 && visited[i] == false)
-				dfs(i);
+
+
+	private static void dfs(int v) {
+		visited[v] = true;
+		sb.append(v + " ");
+		for(int n : lists[v]) {
+			if(!visited[n])
+				dfs(n);
 		}
 	}
 	
-	private static void bfs(int start) {
-		Deque<Integer> deque = new ArrayDeque<>();
-		sb.append(start + " ");
-		visited[start] = true;
-		deque.addFirst(start);
+	private static void bfs(int v) {
+		ArrayDeque<Integer> deque = new ArrayDeque<>();
+		visited[v] = true;
+		deque.add(v);
+		sb.append(v + " ");
 		
 		while(!deque.isEmpty()) {
-			int x = deque.pollFirst();
-			for(int i = 1; i < arr.length; i++) {
-				if(arr[x][i] == 1 && visited[i] == false) {
-					sb.append(i + " ");
-					visited[i] = true;
-					deque.addLast(i);
-					
+			int num = deque.poll();
+			for(int n : lists[num]) {
+				if(!visited[n]) {
+					visited[n] = true;
+					deque.add(n);
+					sb.append(n + " ");
 				}
 			}
 		}
-		
 	}
-
 }
