846851485768fc4520f04c854ed7647a9657e0eb
Yebin Gwak <ferf99@naver.com>
2025-01-27 13:22:47 +0900

    [Silver I] Title: 절댓값 힙, Time: 280 ms, Memory: 25012 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -2,28 +2,25 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 
-		int N = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>((o1, o2) -> {
+		PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
 			if(Math.abs(o1) == Math.abs(o2))
 				return o1 - o2;
-			 return Math.abs(o1) - Math.abs(o2);
-					 });
+			return Math.abs(o1) - Math.abs(o2);
+		});
 		
+		int N = Integer.parseInt(br.readLine());
 		for(int i = 0; i < N; i++) {
-			int x = Integer.parseInt(br.readLine());
-			if(x == 0) {
-				if(!pq.isEmpty())
-					sb.append(pq.poll() + "\n");
-				else 
-					sb.append(0 + "\n");
-				
-			}else {
-				pq.add(x);
-			}
+			int n = Integer.parseInt(br.readLine());
+			if(n == 0)
+				sb.append((pq.isEmpty()) ? 0 : pq.poll()).append("\n");
+			else
+				pq.add(n);
 		}
 		System.out.println(sb.toString());
 	}
