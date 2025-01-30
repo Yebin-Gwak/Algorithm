1332770619cdc0f0c879fcdbc4f74942c3e4751e
Yebin Gwak <ferf99@naver.com>
2025-01-30 16:09:28 +0900

    [Gold IV] Title: 최단경로, Time: 688 ms, Memory: 110992 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,75 +2,76 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static class Node{
-        int v;
-        int cost;
+	
+	static class Node implements Comparable<Node>{
+		int dest;
+		int cost;
+		
+		public Node(int dest, int cost) {
+			this.dest = dest;
+			this.cost = cost;
+		}
+		
+		public int compareTo(Node o) {
+			return this.cost - o.cost;
+		}
+	}
 
-        public Node(int v, int cost) {
-            this.v = v;
-            this.cost = cost;
-        }
-    }
+	static int N, M, K;
+	static List<Node>[] lists;
+	static int[] distances;
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(br.readLine());
+		
+		lists = new ArrayList[N + 1];
+		for(int i = 0; i <= N; i++)
+			lists[i] = new ArrayList<>();
+		distances = new int[N + 1];
+		Arrays.fill(distances, Integer.MAX_VALUE);
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			lists[s].add(new Node(e, cost));
+		}
+		
+		dijkstra();
+		
+		for(int i = 1; i <= N; i++)
+			sb.append((distances[i] == Integer.MAX_VALUE) ? "INF" : distances[i]).append("\n");
+		
+		System.out.print(sb.toString());
+	}
 
-    static ArrayList<Node>[] nodes;
-    static boolean[] visited;
-    static int[] distance;
+	private static void dijkstra() {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		visited = new boolean[N + 1];
+		distances[K] = 0;
+		pq.add(new Node(K, 0));
+		
+		while(!pq.isEmpty()) {
+			Node now = pq.poll();
+			
+			if(!visited[now.dest])
+				visited[now.dest] = true;
+			
+			for(Node next : lists[now.dest]) {
+				if(!visited[next.dest] && distances[now.dest] + next.cost < distances[next.dest]) {
+					distances[next.dest] = distances[now.dest] + next.cost;
+					pq.add(new Node(next.dest, distances[next.dest]));
+				}
+			}
+			
+		}
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(br.readLine());
-
-        nodes = new ArrayList[V + 1];
-        distance = new int[V + 1];
-        visited = new boolean[V + 1];
-
-        for (int i = 1; i <= V; i++) {
-            nodes[i] = new ArrayList<>();
-            distance[i] = Integer.MAX_VALUE;
-        }
-
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int now = Integer.parseInt(st.nextToken());
-            int dest = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-
-            nodes[now].add(new Node(dest, cost));
-        }
-
-        dijkstra(K);
-
-        for (int i = 1; i <= V; i++) {
-        	if(distance[i] == Integer.MAX_VALUE)
-        		System.out.println("INF");
-        	else
-        		System.out.println(distance[i]);
-        }
-    }
-
-    static void dijkstra(int start) {
-    	// Comparator 사용하여 비용 낮은 순으로 우선순위 설정
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
-
-        pq.add(new Node(start, 0));
-        distance[start] = 0;
-
-        while (!pq.isEmpty()) {
-            Node now = pq.poll();
-
-            if (!visited[now.v]) 
-                visited[now.v] = true;
-
-            for (Node next : nodes[now.v]) {
-                if (!visited[next.v] && distance[next.v] > now.cost + next.cost) {
-                    distance[next.v] = now.cost + next.cost;
-                    pq.add(new Node(next.v, distance[next.v]));
-                }
-            }
-        }
-    }
 }
