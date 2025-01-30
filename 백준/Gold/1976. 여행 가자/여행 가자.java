b42378f50cb0706cdc94646763788b397e5654da
Yebin Gwak <ferf99@naver.com>
2025-01-30 12:41:47 +0900

    [Gold IV] Title: 여행 가자, Time: 160 ms, Memory: 17524 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -2,67 +2,54 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N, M;
-	static int[]map;
-	
+
+	static int[] parents;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
-		
-		make();
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		init(N);
 		
 		for(int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 1; j <= N; j++) {
-				int v = Integer.parseInt(st.nextToken());
-				if(v == 1)
+				if(Integer.parseInt(st.nextToken()) == 1)
 					union(i, j);
 			}
 		}
-		
 		st = new StringTokenizer(br.readLine());
-		
-		int start = Integer.parseInt(st.nextToken());
-		start = find(start);
-		boolean check = true;
-		
-		for(int i = 0; i < M - 1; i++) {
-			int v = Integer.parseInt(st.nextToken());
-			if(start != find(v)) {
-				check = false;
-				break;
+		int now = Integer.parseInt(st.nextToken());
+		while(st.hasMoreTokens()) {
+			int next = Integer.parseInt(st.nextToken());
+			if(union(now, next)) {
+				System.out.println("NO");
+				return;
 			}
 		}
-		
-		if(!check)
-			System.out.println("NO");
-		else
-			System.out.println("YES");
+		System.out.print("YES");
 
 	}
-
 	
-	private static void make() {
-		map = new int[N + 1];
+	private static void init(int N) {
+		parents = new int[N + 1];
 		for(int i = 1; i <= N; i++)
-			map[i] = i;
+			parents[i] = i;
+	}
+	
+	private static int find(int a) {
+		if(parents[a] == a)
+			return a;
+		return parents[a] = find(parents[a]);
 	}
 	
 	private static boolean union(int a, int b) {
 		int aRoot = find(a);
 		int bRoot = find(b);
-		if(aRoot == bRoot) return false;
-		map[bRoot] = map[aRoot];
+		if(aRoot == bRoot)
+			return false;
+		parents[bRoot] = parents[aRoot];
 		return true;
-	}
-	
-	private static int find(int a) {
-		if(map[a] == a)
-			return a;
-		return map[a] = find(map[a]);
 	}
 
 }
