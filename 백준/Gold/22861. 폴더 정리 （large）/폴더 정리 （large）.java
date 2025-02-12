52af6be0ddea4ee469063956e1434f6b985b5a46
Yebin Gwak <ferf99@naver.com>
2025-02-12 21:15:42 +0900

    [Gold I] Title: 폴더 정리 (large), Time: 1040 ms, Memory: 202676 KB -BaekjoonHub

--- 백준/Gold/22861. 폴더 정리 （large）/폴더 정리 （large）.java
+++ 백준/Gold/22861. 폴더 정리 （large）/폴더 정리 （large）.java
@@ -3,54 +3,53 @@ import java.io.*;
 
 public class Main {
 	
-	static int N, M;
 	static class Folder{
 		String parent;
 		String name;
-		HashSet<String> folder = new HashSet<>();
-		HashSet<String> file = new HashSet<>();
+		HashSet<String> folders = new HashSet<>();
+		HashSet<String> files = new HashSet<>();
 		
 		public Folder(String parent, String name) {
 			this.parent = parent;
 			this.name = name;
 		}
 		
-		public void insert(String s) {
-			file.add(s);
+		public void insert(String s, int cmd) {
+			if(cmd == 0)
+				files.add(s);
+			else {
+				folders.add(s);
+				computer.get(s).parent = name;
+			}
 		}
 		
 		public void move(String to) {
-			for(String s : folder) {
-				folders.get(s).parent = to;
-				folders.get(to).folder.add(s);
+			for(String s : folders) {
+				computer.get(s).parent = to;
+				computer.get(to).folders.add(s);
 			}
-			for(String s : file)
-				folders.get(to).file.add(s);
-			folder.clear();
-			file.clear();
-			folders.get(parent).folder.remove(name);
+			for(String s : files)
+				computer.get(to).files.add(s);
+			computer.get(parent).folders.remove(name);
 		}
 
-		public void getCount(Result result) {
-			result.cnt += file.size();
-			for(String s : file)
-				result.names.add(s);
+		public void getCount() {
+			cnt += files.size();
+			for(String s : files)
+				set.add(s);
 			
-			for(String s : folder) {
-				if(folders.get(s).parent == null)
+			for(String s : folders) {
+				if(computer.get(s).parent == null)
 					continue;
-				folders.get(s).getCount(result);
+				computer.get(s).getCount();
 			}
 		}
 
 	}
-	
-	static class Result{
-		HashSet<String> names = new HashSet<>();
-		int cnt = 0;
-	}
-	
-	static HashMap<String, Folder> folders = new HashMap<>();
+
+	static HashMap<String, Folder> computer = new HashMap<>();
+	static HashSet<String> set = new HashSet<>();
+	static int cnt = 0;
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -60,40 +59,27 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		folders.put("main", new Folder(null, "main"));
-		
-		ArrayDeque<String[]> deque = new ArrayDeque<>();
-		
+		computer.put("main", new Folder(null, "main"));
+
 		for(int i = 0; i < N + M; i++) {
 			st = new StringTokenizer(br.readLine());
 			String parent = st.nextToken();
 			String name = st.nextToken();
 			int cmd = Integer.parseInt(st.nextToken());
 			
+			computer.putIfAbsent(parent, new Folder(null, parent));
 			if(cmd == 1)
-				folders.put(name, new Folder(parent, name));
-			else
-				deque.add(new String[] {parent, name});
+				computer.putIfAbsent(name, new Folder(null, name));
+			computer.get(parent).insert(name, cmd);
 		}
-		
-		for(Folder f : folders.values()) {
-			if(f.parent == null)
-				continue;
-			folders.get(f.parent).folder.add(f.name);
-		}
-		
-		while(!deque.isEmpty()) {
-			String[] arr = deque.poll();
-			folders.get(arr[0]).insert(arr[1]);
-		}
-		
+	
 		int K = Integer.parseInt(br.readLine());
 		for(int i = 0; i < K; i++) {
 			st = new StringTokenizer(br.readLine());
 			String[] arr1 = st.nextToken().split("/");
 			String[] arr2 = st.nextToken().split("/");
 			
-			Folder from = folders.get(arr1[arr1.length - 1]);
+			Folder from = computer.get(arr1[arr1.length - 1]);
 			String to = arr2[arr2.length - 1];
 			
 			from.move(to);
@@ -101,13 +87,13 @@ public class Main {
 		
 		int Q = Integer.parseInt(br.readLine());
 		for(int i = 0; i < Q; i++) {
-			
 			String[] arr = br.readLine().split("/");
 			String folder = arr[arr.length - 1];
+			set.clear();
+			cnt = 0;
 			
-			Result result = new Result();
-			folders.get(folder).getCount(result);
-			sb.append(result.names.size() + " " + result.cnt).append("\n");
+			computer.get(folder).getCount();
+			sb.append(set.size() + " " + cnt).append("\n");
 		}
 		
 		System.out.print(sb.toString());
