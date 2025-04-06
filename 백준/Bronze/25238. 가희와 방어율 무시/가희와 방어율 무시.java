ab898828cd7c83ff6bdac24d7afce3bf1d6fcb73
Yebin Gwak <ferf99@naver.com>
2025-04-06 22:07:16 +0900

    [Bronze IV] Title: 가희와 방어율 무시, Time: 104 ms, Memory: 14036 KB -BaekjoonHub

---
+++ 백준/Bronze/25238. 가희와 방어율 무시/가희와 방어율 무시.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int ans = a * (100 - b);
+		System.out.println((ans >= 10000) ? 0 : 1);
+	}
+
+}
