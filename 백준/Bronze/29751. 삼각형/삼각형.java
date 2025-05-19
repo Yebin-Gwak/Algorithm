ae04356fa47b041e9f1b46d5975f64403a33e0f4
Yebin Gwak <ferf99@naver.com>
2025-05-19 12:43:22 +0900

    [Bronze V] Title: 삼각형, Time: 104 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int W = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
+		double S = W * H / 2.0;
+		System.out.println(S);
+	}
+
+}
