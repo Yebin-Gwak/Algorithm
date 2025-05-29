5593e1702be7f866090366e6f457ba5a8e392f66
Yebin Gwak <ferf99@naver.com>
2025-05-29 15:46:25 +0900

    [Platinum II] Title: SCP-504, Time: 1680 ms, Memory: 337240 KB -BaekjoonHub

--- 백준/Platinum/27946. SCP－504/SCP－504.java
+++ 백준/Platinum/27946. SCP－504/SCP－504.java
@@ -1,119 +1,63 @@
 import java.io.*;
-
 import java.util.*;
 
 public class Main {
 
-   static class Trie{
-
-      HashMap<String, Integer> map = new HashMap<>();
-
-      StringBuilder sb = new StringBuilder();
-
-      boolean[] isFirst = new boolean[128];
-
-      
-
-      public void insert(String s) {
-
-         sb.setLength(0);
-
-         Arrays.fill(isFirst, true);
-
-         for(int i = 0; i < s.length(); i++) {
-
-            char c = s.charAt(i);
-
-            if(!isFirst[c])
-
-               continue;
-
-            isFirst[c] = false;
-
-            sb.append(c).append('.').append(i).append('.');
-
-         }
-
-         String key = sb.toString();
-
-         map.put(key, map.getOrDefault(key, 0) + 1);
-
-         
-
-      }
-
-      
-
-      public int find(String s) {
-
-         sb.setLength(0);
-
-         Arrays.fill(isFirst, true);
-
-         int cnt = 0;
-
-         
-
-         for(int i = 0; i < s.length(); i++) {
-
-            char c = s.charAt(i);
-
-            if(!isFirst[c])
-
-               continue;
-
-            isFirst[c] = false;
-
-            sb.append(c).append('.').append(i).append('.');
-
-            String key = sb.toString();
-
-            cnt += map.getOrDefault(key, 0);
-
-         }
-
-         
-
-         return cnt;
-
-      }
-
-   }
-
-   
-
-   public static void main(String[] args) throws IOException{
-
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-      StringTokenizer st = new StringTokenizer(br.readLine());
-
-      StringBuilder sb = new StringBuilder();
-
-      
-
-      int N = Integer.parseInt(st.nextToken());
-
-      int M = Integer.parseInt(st.nextToken());
-
-      Trie trie = new Trie();
-
-      
-
-      for(int i = 0; i < N; i++)
-
-         trie.insert(br.readLine());
-
-      
-
-      for(int i = 0; i < M; i++)
-
-         sb.append(trie.find(br.readLine())).append("\n");
-
-      
-
-      System.out.print(sb.toString());
-
-   }
+	static class Trie{
+		HashMap<String, Integer> map = new HashMap<>();
+		StringBuilder sb = new StringBuilder();
+		boolean[] isFirst = new boolean[128];
+		
+		public void insert(String s) {
+			sb.setLength(0);
+			Arrays.fill(isFirst, true);
+			for(int i = 0; i < s.length(); i++) {
+				char c = s.charAt(i);
+				if(!isFirst[c])
+					continue;
+				isFirst[c] = false;
+				sb.append(c).append(i).append('.');
+			}
+			String key = sb.toString();
+			map.put(key, map.getOrDefault(key, 0) + 1);
+			
+		}
+		
+		public int find(String s) {
+			sb.setLength(0);
+			Arrays.fill(isFirst, true);
+			int cnt = 0;
+			
+			for(int i = 0; i < s.length(); i++) {
+				char c = s.charAt(i);
+				if(!isFirst[c])
+					continue;
+				isFirst[c] = false;
+				sb.append(c).append(i).append('.');
+				String key = sb.toString();
+				cnt += map.getOrDefault(key, 0);
+			}
+			
+			return cnt;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		Trie trie = new Trie();
+		
+		for(int i = 0; i < N; i++)
+			trie.insert(br.readLine());
+		
+		for(int i = 0; i < M; i++)
+			sb.append(trie.find(br.readLine())).append("\n");
+		
+		System.out.print(sb.toString());
+	}
 
 }
