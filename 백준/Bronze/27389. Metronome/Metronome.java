266a1955cbfb727df7e9f6c0e89bf92f61e5f664
Yebin Gwak <ferf99@naver.com>
2025-06-28 07:03:41 +0900

    [Bronze V] Title: Metronome, Time: 112 ms, Memory: 14484 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.printf("%.2f", Integer.parseInt(br.readLine()) / 4.00);
+	}
+
+}
