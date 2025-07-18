c1b00826877d31478c8834b1c8b89cb83cacec6f
Yebin Gwak <ferf99@naver.com>
2025-07-18 21:41:03 +0900

    [Bronze V] Title: Rectangles, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		String s = "*".repeat(M) + "\n";
+		System.out.print(s.repeat(N));
+	}
+}
