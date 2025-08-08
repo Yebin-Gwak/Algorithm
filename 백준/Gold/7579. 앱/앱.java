44ad0d6a607ad9f94ba0d05b5118da92bf144bcf
Yebin Gwak <ferf99@naver.com>
2025-08-08 15:19:56 +0900

    [Gold III] Title: 앱, Time: 124 ms, Memory: 14460 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -1,37 +1,40 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		int[][] apps = new int[N][2];
-
-		for (int i = 0; i < 2; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++)
-				apps[j][i] = Integer.parseInt(st.nextToken());
+		
+		int[] wArr = new int[N];
+		int[] vArr = new int[N];
+		int max = 0;
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++)
+			vArr[i] = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			wArr[i] = Integer.parseInt(st.nextToken());
+			max += wArr[i];
 		}
 		
-		int maxC = 0;
-		for(int[] n : apps)
-			maxC += n[1];
-
-		int[] dp = new int[maxC + 1];
-		for (int i = 0; i < N; i++) {
-			for (int j = maxC; j >= apps[i][1]; j--)
-				dp[j] = Math.max(dp[j], dp[j - apps[i][1]] + apps[i][0]);
+		int[] dp = new int[max + 1];
+		for(int i = 0; i < N; i++) {
+			int w = wArr[i];
+			int v = vArr[i];
+			for(int j = max; j >= w; j--)
+				dp[j] = Math.max(dp[j], dp[j - w] + v);
 		}
-
-		for (int i = 0; i <= maxC; i++) {
-			if (dp[i] >= M) {
+		
+		for(int i = 0; i <= max; i++) {
+			if(dp[i] >= M) {
 				System.out.println(i);
 				return;
 			}
 		}
-
+		
 	}
 }
