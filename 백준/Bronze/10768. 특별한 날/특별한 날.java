ab1ec426a21f05af57fe400e13f0fb5743369810
Yebin Gwak <ferf99@naver.com>
2025-09-20 14:19:55 +0900

    [Bronze IV] Title: 특별한 날, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int m = Integer.parseInt(br.readLine());
+		int d = Integer.parseInt(br.readLine());
+		if(m == 2 && d == 18)
+			System.out.println("Special");
+		else if(m == 1 || (m == 2 && d < 18))
+			System.out.println("Before");
+		else
+			System.out.println("After");
+	}
+}
