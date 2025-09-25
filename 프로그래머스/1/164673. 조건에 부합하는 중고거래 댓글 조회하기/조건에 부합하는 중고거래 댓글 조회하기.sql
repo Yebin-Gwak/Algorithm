8e4f0709169ef29272c9840e0d1d99284fed419c
Yebin Gwak <ferf99@naver.com>
2025-09-25 22:46:47 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT b.TITLE, b.BOARD_ID, r.REPLY_ID, r.WRITER_ID, r.CONTENTS, DATE_FORMAT(r.CREATED_DATE, '%Y-%m-%d')
+from USED_GOODS_BOARD b
+join USED_GOODS_REPLY r
+    on b.BOARD_ID = r.BOARD_ID
+where b.CREATED_DATE between '2022-10-01' and '2022-10-31'
+order by r.CREATED_DATE, b.TITLE
