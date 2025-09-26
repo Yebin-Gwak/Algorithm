566e738c9c55033d75664f61ee2f25187d490389
Yebin Gwak <ferf99@naver.com>
2025-09-27 02:20:44 +0900

    [level 3] Title: 입국심사, Time: 72.28 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,39 +1,28 @@
 import java.util.*;
-// 3:00
 
 class Solution {
-    static int N;
-    static int[] times;
+
     public long solution(int n, int[] times2) {
-        N = n;
-        times = times2;
-        
-        Arrays.sort(times2);
-        long MAX_VALUE = 1000000000;
-        long min = 0;
-        long max = MAX_VALUE * MAX_VALUE;
-        long answer = 0;
-        
+        long min = 1;
+        long max = Long.MAX_VALUE;
+        for(int m : times2)
+            max = Math.min(max, m);
+        max *= n;
+        long ans = max;
         while(min <= max){
             long mid = (min + max) / 2;
-            long pass = binarySearch(mid);
-            
-            if(pass >= n){
-                answer = mid;
+            long cnt = 0;
+            for(int m : times2)
+                cnt += mid / m;
+            if(cnt >= n){
+                ans = mid;
                 max = mid - 1;
-            }else{
+            }else
                 min = mid + 1;
-            }
         }
-  
-        return answer;
+        
+        return ans;
     }
     
-    private long binarySearch(long time){
-        long pass = 0;
-        for(int i = 0; i < times.length; i++){
-            pass += time / (long) times[i];
-        }
-        return pass;
-    }
+
 }
