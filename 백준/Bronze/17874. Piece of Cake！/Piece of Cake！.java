4cdec9b7ba7ca6e5c4c43679f85c09d100420b0b
Yebin Gwak <ferf99@naver.com>
2025-10-29 20:24:39 +0900

    [Bronze IV] Title: Piece of Cake!, Time: 100 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/17874. Piece of Cake！/Piece of Cake！.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		A = Math.max(A, N - A);
+		B = Math.max(B, N - B);
+		System.out.println(A * B * 4);
+	}
+}
