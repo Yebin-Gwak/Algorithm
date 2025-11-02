e9a945746addbdd43b512c9f40df470f9ff4f195
Yebin Gwak <ferf99@naver.com>
2025-11-03 01:55:18 +0900

    [Bronze IV] Title: Deliv-e-droid, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/28248. Deliv－e－droid/Deliv－e－droid.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		int ans = N * 50 + M * -10;
+		if(N > M) ans += 500;
+		System.out.println(ans);
+	}
+}
