ae273c217128235ac62e96dbae2d1404bb7b81d6
Yebin Gwak <ferf99@naver.com>
2025-11-16 23:41:32 +0900

    [Bronze IV] Title: Judging Moose, Time: 124 ms, Memory: 15836 KB -BaekjoonHub

---
+++ 백준/Bronze/15025. Judging Moose/Judging Moose.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		if(a == 0 && b == 0)
+			System.out.println("Not a moose");
+		else {
+			if(a == b) 
+				System.out.println("Even " + (a + b));
+			else
+				System.out.println("Odd " + (Math.max(a, b) * 2));
+		}
+			
+	}
+}
