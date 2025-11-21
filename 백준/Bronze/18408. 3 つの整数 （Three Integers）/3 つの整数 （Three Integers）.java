52efafdf29b1d2c6b040765c5c999244d2cb2060
Yebin Gwak <ferf99@naver.com>
2025-11-21 21:46:54 +0900

    [Bronze IV] Title: 3 つの整数 (Three Integers), Time: 100 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/18408. 3 つの整数 （Three Integers）/3 つの整数 （Three Integers）.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int o = 0;
+		int t = 0;
+		for(int i = 0; i < 3; i++) {
+			if(Integer.parseInt(st.nextToken()) == 1)
+				o++;
+			else
+				t++;
+		}
+		System.out.println(o > t ? 1 : 2);
+			
+	}
+}
