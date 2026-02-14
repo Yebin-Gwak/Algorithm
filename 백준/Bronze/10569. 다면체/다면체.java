c883b0c8efda46a4df4a99181383208a87bc1b81
Yebin Gwak <ferf99@naver.com>
2026-02-14 09:44:57 +0900

    [Bronze IV] Title: 다면체, Time: 108 ms, Memory: 14576 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		while(T-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(2 - a + b + "\n");
+		}
+		System.out.println(sb);
+	}
+}
